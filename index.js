ed332a4539e1d057052d9ed803f29c1bcc10189f
Usman Khan <88879256+usman-khan98@users.noreply.github.com>
2023-02-08 16:57:48 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
+
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
 }
